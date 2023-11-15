2c6f81c816ec0dee5db33480047c07e295eac80d
Yura Movsesyan <movsesyan-03@inbox.ru>
2023-11-15 20:14:31 +0400

    added class Fraction

---
+++ lib/src/fraction.dart
@@ -0,0 +1,62 @@
+import 'package:twopp_lib/src/helper.dart';
+
+class Fraction {
+  int numerator, denominator;
+
+  Fraction(this.numerator, this.denominator) {
+    _cut();
+  }
+  Fraction.num(int num) : this(num, 1);
+  Fraction.zero() : this.num(0);
+
+  double getResult() => numerator / denominator;
+
+  Fraction _cut() {
+    if (denominator < 0) {
+      numerator *= -1;
+      denominator *= -1;
+    }
+
+    int nod = gcd(numerator, denominator);
+
+    if (nod == 1) return this;
+
+    numerator ~/= nod;
+    denominator ~/= nod;
+
+    return this;
+  }
+
+  Fraction cut() => Fraction(numerator, denominator)._cut();
+
+  Fraction operator +(Fraction other) => Fraction(
+        numerator * other.denominator + other.numerator * denominator,
+        denominator * other.denominator,
+      );
+
+  Fraction operator -(Fraction other) => Fraction(
+        numerator * other.denominator - other.numerator * denominator,
+        denominator * other.denominator,
+      );
+
+  Fraction operator *(Fraction other) => Fraction(
+        numerator * other.numerator,
+        denominator * other.denominator,
+      );
+
+  Fraction operator /(Fraction other) => Fraction(
+        numerator * other.denominator,
+        denominator * other.numerator,
+      );
+
+  @override
+  String toString() {
+    if (numerator == 0) return "0";
+
+    if (denominator == 1) {
+      return "$numerator";
+    } else {
+      return "$numerator/$denominator";
+    }
+  }
+}
